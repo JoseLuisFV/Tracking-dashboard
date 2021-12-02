2a34f6f2319971d06d371ff3f017c8a9e6e47bf6
BIOZ <josefvacio@gmail.com>
2021-12-02 14:15:54 -0800

    Se agrego el contenido mediante js y se iniciaron algunos estilos basicos

--- dist/app.js
+++ dist/app.js
@@ -9,8 +9,49 @@ const getData = (path) => {
 const createApp = async() => {
     const data = await getData("./data.json")
     data.forEach(item => {
-        console.log(item.title);
+        createTrack(item)
     });
+    // item.title
+    // item.timeframes.daily
+    // item.timeframes.daily.current
+    // item.timeframes.daily.previous
+    // item.timeframes.weekly
+    // item.timeframes.monthly
+}
+
+const createTrack = (item) => {
+    const activity = document.createElement('article');
+    activity.className = `${item.title.toLowerCase().split(' ').join('-')}`;
+    activity.classList.add('activity')
+
+    const wrapper = document.createElement('div');
+    wrapper.className = `activity__wrapper`;
+    addContent(item, wrapper);
+    activity.appendChild(wrapper);
+
+    const trackDashboard = document.getElementById('track');
+    trackDashboard.appendChild(activity)
+}
+
+const addContent = (item, wrapper) => {
+    const nodes = []
+    const title = document.createElement('h2');
+    title.textContent = item.title
+
+    const current = document.createElement('span');
+    current.textContent = `${item.timeframes.weekly.current}hrs`
+
+    const menu = document.createElement('div');
+    menu.className = `activity__icon`
+
+    const previous = document.createElement('span');
+    previous.textContent = `last week - ${item.timeframes.weekly.previous}hrs`
+    
+    nodes.push(title);
+    nodes.push(current);
+    nodes.push(menu);
+    nodes.push(previous)
+    wrapper.append(...nodes)
 }
 
 createApp();
